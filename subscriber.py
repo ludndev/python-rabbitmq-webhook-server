9b618388c75923935c11a8b05a1948f3b1564b99
ludndev <ludndev@gmail.com>
2024-10-08 12:53:08 +0100

    feat: add subscriber.py

---
+++ subscriber.py
@@ -0,0 +1,70 @@
+import time
+import time
+import pika
+import requests
+import json
+from payload_dto import PayloadDto
+from producer import Producer
+from config import Config
+
+class Subscriber:
+    ACCEPTED_HEADERS = [200, 201]
+
+    def __init__(self, queue_name):
+        self.connection = self.conn()
+        self.channel = self.connection.channel()
+        self.queue_name = queue_name
+        self.channel.queue_declare(queue=self.queue_name)
+        self.producer = Producer()
+
+    def conn(self):
+        config = pika.ConnectionParameters(host=Config.RABBITMQ_HOST, port=Config.RABBITMQ_PORT)
+        return pika.BlockingConnection(config)
+
+    def process(self, ch, method, properties, body):
+        headers = {
+            'user-agent': Config.USER_AGENT,
+        }
+
+        payload = json.loads(body)
+        dto = PayloadDto(**payload)
+
+        # make thread blocking
+        time.sleep(10)
+
+        if 'headers' in payload and len(payload['headers']) > 0:
+            headers.update(payload['headers'])
+
+        try:
+            response = requests.post(dto.url, json=dto.body, headers=dto.headers)
+
+            if response.status_code in self.ACCEPTED_HEADERS:
+                self.emit_callback(dto.event, response.status_code)
+        except Exception as e:
+            print(f"Error processing message: {e}")
+
+    def emit_callback(self, event: str, status: int):
+        self.producer.channel.queue_declare(queue=Config.CALLBACK_QUEUE)
+
+        callback_message = {
+            'webhook_id': 'X1234567890',  # oid/uuid/nanoid
+            'event': event,
+            'status': status,
+            'at': time.time(),
+        }
+
+        self.producer.channel.basic_publish(
+            exchange='',
+            routing_key=Config.CALLBACK_QUEUE,
+            body=json.dumps(callback_message)
+        )
+
+        print(f"Callback Sent: {callback_message}")
+
+    def start(self):
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.process, auto_ack=True)
+        print(f'Waiting for messages on {self.queue_name}. To exit press CTRL+C')
+        self.channel.start_consuming()
+
+    def close(self):
+        self.connection.close()
