40060d6ba3e216012839150a7a41fd36294f736d
ludndev <ludndev@gmail.com>
2024-10-08 12:51:55 +0100

    feat: add producer.py

---
+++ producer.py
@@ -0,0 +1,24 @@
+import pika
+from payload_dto import PayloadDto
+from config import Config
+
+class Producer:
+    def __init__(self):
+        self.connection = self.conn()
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=Config.WEBHOOK_QUEUE)
+
+    def conn(self):
+        config = pika.ConnectionParameters(host=Config.RABBITMQ_HOST, port=Config.RABBITMQ_PORT)
+        return pika.BlockingConnection(config)
+
+    def produce_message(self, payload: PayloadDto):
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=Config.WEBHOOK_QUEUE,
+            body=payload.to_json()
+        )
+        print(f"Sent: {payload.to_json()}")
+
+    def close(self):
+        self.connection.close()
