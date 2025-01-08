2379c97abf2a6814ebf91bfa1518d6a185865778
ludndev <ludndev@gmail.com>
2025-01-08 19:13:05 +0100

    refact: load parent folder class and logic from examples folder

--- examples/main_callback.py
+++ examples/main_callback.py
@@ -1,3 +1,20 @@
+import sys
+import os
+
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+
+# adding the parent directory to 
+# the sys.path.
+sys.path.append(parent)
+
+# ---
+
 from config import Config
 from subscriber import Subscriber
 import json
